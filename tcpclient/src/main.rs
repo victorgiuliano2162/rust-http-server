87b06218ec648121a6a358317476f837e6513e65
Victor Giuliano <vitor.giuliano@gmail.com>
2024-07-24 01:38:10 -0300

    listener return

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -1,6 +1,14 @@
+use core::str;
+use std::io::{Read, Write};
 use std::net::TcpStream;
 
 fn main() {
-    let _stream = TcpStream::connect("localhost:3000").unwrap();
-    
+    let mut stream = TcpStream::connect("localhost:3000").unwrap();
+    stream.write("Hello".as_bytes()).unwrap();
+    let mut buffer = [0;5];
+    stream.read(&mut buffer).unwrap();
+    println!(
+        "Got response from server: {:?}",
+        str::from_utf8(&buffer).unwrap()
+    );
 }
