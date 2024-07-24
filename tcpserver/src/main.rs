28edee0f2af617d4cd1cb8aeb5a5bd8aca6258c2
Victor Giuliano <vitor.giuliano@gmail.com>
2024-07-24 01:38:22 -0300

    a

--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::{Read, Write}, net::TcpListener};
 
 fn main() {
     let connetion_listener = TcpListener::bind("127.0.0.1:3000").unwrap();
@@ -6,9 +6,12 @@ fn main() {
     println!("Running on port 3000");
 
     for stream in connetion_listener.incoming() {
-        let _stream = stream.unwrap();
+        let mut stream = stream.unwrap();
         println!("Connetion established");
+        let mut buffer =  [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        stream.write(&mut buffer).unwrap();
     }
 
-    
+
 }
