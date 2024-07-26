6e1029c68c92e4dd6eac6b8e1345fc96e90c73c8
Victor Giuliano <vitor.giuliano@gmail.com>
2024-07-26 14:29:09 -0300

    HTTP response default awnser

---
+++ http/src/httpresponse.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+use std::io::{Result, Write};
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct HttpResponse<'a> {
+    version: &'a str,
+    status_code: &'a str,
+    status_text: &'a str,
+    headers: Option<HashMap<&'a str, &'a str>>,
+    body: Option<String>,
+}
+
+impl <'a> Default for HttpResponse<'a> {
+    fn default() -> Self {
+        Self {
+            version: "HTTP/1.1".into(),
+            status_code: "200".into(),
+            status_text: "OK".into(),
+            headers: None,
+            body: None,
+        }
+    }
+}
