b0dd1cba1bb4ccf2ce2d38b91bdc207af5d36129
Victor Giuliano <vitor.giuliano@gmail.com>
2024-07-26 21:26:52 -0300

    server run and new methods

---
+++ httpserver/src/server.rs
@@ -0,0 +1,28 @@
+use super::router::Router;
+use http::httprequest::HttpRequest;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::str;
+
+pub struct Server<'a> {
+    socket_addr: &'a str,
+}
+impl<'a> Server<'a> {
+    pub fn new(socket_addr: &'a str) -> Self {
+        Server { socket_addr }
+    }
+    pub fn run(&self) {
+        //start a server running o socket address
+        let connection_listener = TcpListener::bind(self.socket_addr).unwrap();
+        println!("Running on {}", self.socket_addr);
+        //Listen to incoming connections in a loop
+        for stream in connection_listener.incoming() {
+            let mut stream = stream.unwrap();
+            println!("Connection established");
+            //Convert HTTP request to Rust data structure
+            let req: HttpRequest = String::from_utf8(read_buffer.to_vec()).unwrap().into();
+            //Route request to appropriate handler
+            Router::route(req, &mut stream);
+        }
+    }
+} 
