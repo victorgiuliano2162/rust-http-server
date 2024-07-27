da04e83307becec5d1123572c99bbff8a94fe6a9
Victor Giuliano <vitor.giuliano@gmail.com>
2024-07-27 00:33:25 -0300

    implement WebService and handler for webservice

--- httpserver/src/handler.rs
+++ httpserver/src/handler.rs
@@ -2,7 +2,7 @@ use http::{httprequest::HttpRequest, httpresponse::HttpResponse};
 use serde::{Deserialize, Serialize};
 
 use std::collections::HashMap;
-use std::{env, fs};
+use std::{default, env, fs};
 
 pub trait Handler {
     fn handle(red: &HttpRequest) -> HttpResponse;
@@ -55,6 +55,35 @@ impl Handler for StaticPageHandler {
 }
 pub struct PageNotFoundHandler;
 pub struct WebServiceHandler;
+impl WebServiceHandler {
+    fn load_json() -> Vec<OrderStatus> {
+        let default_path = format!("{}/data", env!("CARGO_MANIFEST_DIR"));
+        let data_path = env::var("DATA_PATH").unwrap_or(default_path);
+        let full_path = format!("{}/{}", data_path, "orders.json");
+        let json_contents = fs::read_to_string(full_path);
+        let orders: Vec<OrderStatus> = serde_json::from_str(json_contents.unwrap().as_str()).unwrap();
+        orders
+    }
+}
+
+impl Handler for WebServiceHandler {
+    fn handle(req: &HttpRequest) -> HttpResponse {
+        let http::httprequest::Resource::Path(s) = &req.resource;
+
+        //parse the uri
+        let route Vec<&str> = s.split("/").collect();
+        //if route /api/shipping/orders return json
+        match route[2] {
+            "shipping" if route.len() > 2 && route[3] == "orders" => {
+                let body = Some(serde_json::to_string(&Self::load_json()).unwrap());
+                let mut headers: HashMap<&str, &str> = HashMap::new();
+                headers.insert("Content-Type", "application/json");
+                HttpResponse::new("200", Some(headers), body)
+            }
+            _ => HttpResponse::new("404", None, Self::load_file("404.html")),
+        }
+    }
+}
 
 impl Handler for PageNotFoundHandler {
     fn handle(_req: &HttpRequest) -> HttpResponse {
